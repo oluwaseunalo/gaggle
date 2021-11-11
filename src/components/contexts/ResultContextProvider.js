a4fd09f7d5f7e2ea2ea6726e942f7b576c04b024
Oluwaseun Alo <oluwaseunalo4@gmail.com>
2021-11-11 04:24:35 +0100

    result: Complete the result page

--- src/components/contexts/ResultContextProvider.js
+++ src/components/contexts/ResultContextProvider.js
@@ -6,7 +6,7 @@ const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
 export const ResultContextProvider = ({children}) => {
         const [results, setResults] = useState([]);
         const [isLoading, setIsLoading] = useState(false)
-        const [searchTerm, setSearchTerm] = useState('');
+        const [searchTerm, setSearchTerm] = useState('elon musk');
 
 
 const getResults = async (type) => {
@@ -23,9 +23,18 @@ const getResults = async (type) => {
 
     const data = await response.json();
 
+
+    if (type.includes('/news')){
+        setResults(data.entries);
+    } else if (type.includes('/images')) {
+        setResults(data.image_results);
+    } else {
+        setResults(data.results);
+    }
+
     console.log(data);
 
-    setResults(data);
+    
     setIsLoading(false)
 
 }
