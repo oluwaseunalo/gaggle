0bfdb58025d95949864bfc99d8b96ccd76960094
oluwaseunalo <oluwaseunalo4@gmail.com>
2021-11-15 01:22:43 +0100

    query: Complete the search component

---
+++ src/components/Links.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const links = [
+  { url: "/search", text: "🔎 All" },
+  { url: "/news", text: "📰 News" },
+  { url: "/images", text: "🖼️ Images" },
+  { url: "/videos", text: "📹 Videos" },
+];
+
+const Links = () => {
+  return (
+    <div className="flex sm: justify-around justify-between items-center mt-4">
+      {links.map(({ url, text }) => (
+        <NavLink
+          to={url}
+          className="m-2"
+          activeClassName="text-blue-700 border-b-2 dark: text-blue-700 pb-2"
+        >
+          {text}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
+
+export default Links;
--- src/components/contexts/ResultContextProvider.js
+++ src/components/contexts/ResultContextProvider.js
@@ -1,51 +1,47 @@
-import { createContext, useContext, useState} from "react";
+import React from "react";
+import { createContext, useContext, useState } from "react";
 
 const ResultContext = createContext();
-const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
 
-export const ResultContextProvider = ({children}) => {
-        const [results, setResults] = useState([]);
-        const [isLoading, setIsLoading] = useState(false)
-        const [searchTerm, setSearchTerm] = useState('elon musk');
+export const ResultContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("elon musk");
 
-
-const getResults = async (type) => {
+  const getResults = async (type) => {
     setIsLoading(true);
 
     const response = await fetch(`${baseUrl}${type}`, {
-        method: 'Get',
-        headers: {
-            'x-user-agent': 'desktop',
-            'x-rapidapi-host': 'google-search3.p.rapidapi.com',
-            'x-rapidapi-key': 'ca1f4e7103mshf62990c8b796226p19cc5ejsnfd1f386798de'
-        }
+      method: "Get",
+      headers: {
+        "x-user-agent": "desktop",
+        "x-rapidapi-host": "google-search3.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+      },
     });
 
     const data = await response.json();
 
-
-    if (type.includes('/news')){
-        setResults(data.entries);
-    } else if (type.includes('/images')) {
-        setResults(data.image_results);
+    if (type.includes("/news")) {
+      setResults(data.entries);
+    } else if (type.includes("/images")) {
+      setResults(data.image_results);
     } else {
-        setResults(data.results);
+      setResults(data.results);
     }
 
     console.log(data);
 
-    
-    setIsLoading(false)
-
-}
-return (
-    <ResultContext.Provider value={{getResults, results, searchTerm,setSearchTerm, isLoading}}>
-        {children}
+    setIsLoading(false);
+  };
+  return (
+    <ResultContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}
+    >
+      {children}
     </ResultContext.Provider>
-);
-
-}
+  );
+};
 
 export const useResultContext = () => useContext(ResultContext);
-
-
