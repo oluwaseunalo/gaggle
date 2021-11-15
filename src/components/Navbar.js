1572481e5fd4ab7cdd4393b4a34b3ff7c8130f0c
oluwaseunalo <oluwaseunalo4@gmail.com>
2021-11-15 01:46:16 +0100

    fix: Abug fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Search  from './Search'
+import { Search } from "./Search";
 
-const Navbar = ({darkTheme, setDarkTheme}) => {
-    return (
-        <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-70 border-gray-200">
-            
-            <div className="flex justify-between items-center space-x-5 w-screen">
-                <Link to="/">
-                    <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900">
-                            Gaggle 🔎
-                    </p>
-                </Link>
-                <button type="button" onClick={()=> setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white rounded-full px-2 py-1 hover:shadow-lg">
-                        {darkTheme ? "Light 💡" : "Dark 🌙" }
-                </button>
-            </div>
-            <Search />
-        </div>
-    )
-}
+const Navbar = ({ darkTheme, setDarkTheme }) => {
+  return (
+    <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-70 border-gray-200">
+      <div className="flex justify-between items-center space-x-5 w-screen">
+        <Link to="/">
+          <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900">
+            Gaggle 🔎
+          </p>
+        </Link>
+        <button
+          type="button"
+          onClick={() => setDarkTheme(!darkTheme)}
+          className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white rounded-full px-2 py-1 hover:shadow-lg"
+        >
+          {darkTheme ? "Light 💡" : "Dark 🌙"}
+        </button>
+      </div>
+      <Search />
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
